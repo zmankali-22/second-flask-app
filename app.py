b4a57e9a71d81dab0f1bad864a61819155bec01b
utsav <sav.acharya22@gmail.com>
2024-02-01 18:23:57 +1100

    add new product

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 
 
-from flask import Flask
+from flask import Flask, request
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://practice_dev:123456@localhost:5432/practice_database"
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Product(db.Model):
 
@@ -21,7 +23,18 @@ class Product(db.Model):
     stock = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(1000))
 
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "price", "stock", "description")
 
+
+# handle all products
+products_schema = ProductSchema(many=True)
+
+
+#  handle singlr prodcuts
+
+product_schema = ProductSchema(many=False)
 
 @app.cli.command("create")
 def create_table():
@@ -46,3 +59,33 @@ def drop_table():
     db.drop_all()
     print("Table dropped")  
 
+
+@app.route("/products")
+def get_products():
+    stmt = db.select(Product)
+    product_list = db.session.scalars(stmt)
+    # non serializable to json
+    data = products_schema.dump(product_list)
+    return data
+
+@app.route("/products/<int:product_id>")
+def get_product(product_id):
+    stmt = db.select(Product).filter_by(id=product_id)
+    product = db.session.scalar(stmt)
+    # non serializable to json
+
+    if(product):
+
+        data = product_schema.dump(product)
+        return data
+    else:
+        return {"message": "Product not found"}, 404
+    
+@app.route("/products", methods=["POST"])
+def create_product():
+    product_fileds = request.get_json()
+    new_product = Product(name = product_fileds.get("name"), price = product_fileds.get("price"), stock = product_fileds.get("stock"), description = product_fileds.get("description"))
+    db.session.add(new_product)
+    db.session.commit()
+    data = product_schema.dump(new_product)
+    return data, 201
